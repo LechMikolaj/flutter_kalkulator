98c8a82d0b753fdc3dd76fe361029aad95b12f54
LechMikolaj <lech_mikolaj@interia.pl>
2023-03-31 22:02:35 +0200

    Change type of showing results.

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Calculator'),
     );
   }
 }
@@ -26,11 +26,19 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+void count()
+{
+
+}
 final aField = TextEditingController();
 final bField = TextEditingController();
 final cField = TextEditingController();
 
 class _MyHomePageState extends State<MyHomePage> {
+  late double _deltaNumber=0.0;
+  late double _x1Number=0.0;
+  late double _x2Number=0.0;
+  late String _content="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,33 +52,39 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children:  <Widget>[
-                const Text("y="),
+                const Text("y=", style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 30)),
                 SizedBox(
-                  width:41,
+                  width:45,
+                  height:40,
                   child:TextField(
                     controller: aField,
+                    style: TextStyle(fontSize: 20, height: 2.3, color: Colors.black),
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'A:',
                     ),
                   ),
                 ),
-                const Text("x^2+"),
+                const Text("x²+",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 30)),
                 SizedBox(
-                  width:41,
+                  width:45,
+                  height:40,
                   child:TextField(
                     controller: bField,
+                    style: TextStyle(fontSize: 20, height: 2.3, color: Colors.black),
                     decoration: const InputDecoration(
                       border: const OutlineInputBorder(),
                       hintText: 'B:',
                     ),
                   ),
                 ),
-                const Text("+"),
+                const Text("x+",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 30)),
                 SizedBox(
-                  width:41,
+                  width:45,
+                  height:40,
                   child:TextField(
                     controller: cField,
+                    style: TextStyle(fontSize: 20, height: 2.3, color: Colors.black),
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'C:',
@@ -80,8 +94,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             RaisedButton(
-                color: Colors.red, // background
-                textColor: Colors.white, // foreground
+              color: Colors.red, // background
+              textColor: Colors.white, // foreground
               onPressed: () {
                 var content = "";
                 try {
@@ -95,28 +109,87 @@ class _MyHomePageState extends State<MyHomePage> {
                   if (aNumber != 0 && delta >= 0) {
                     // ignore: prefer_interpolation_to_compose_strings
                     content = "delta=" + delta.toString() + '\n' + "x1=" + x1.toString() + '\n' + "x2=" + x2.toString();
+                    setState(()
+                        {
+                          _content = content;
+                        });
                   }
                   else {
                     content = "delta=$delta";
+                    setState(()
+                    {
+                      _content = content;
+                    });
                   }
                 }
                 on Exception catch (e) {
                   content="enter numbers not text";
+                  setState(()
+                  {
+                    _content = content;
+                  });
                 }
-                showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      content: Text(content),
-                    );
-                  },
-                );
+                // showDialog(
+                //   context: context,
+                //   builder: (context) {
+                //     return AlertDialog(
+                //       content: Text(content),
+                //     );
+                //   },
+                // );
                 aField.clear();
                 bField.clear();
                 cField.clear();
               },
               child: const Text('Calculate'),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  <Widget>[
+                  Text(_content,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+                  ),
+                  // Text(_deltaNumber.toString(),
+                  //   textAlign: TextAlign.center,
+                  //   overflow: TextOverflow.ellipsis,
+                  //   style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+                  // ),
+                ],
+            ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children:  <Widget>[
+            //     Text('x1=',
+            //       textAlign: TextAlign.center,
+            //       overflow: TextOverflow.ellipsis,
+            //       style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+            //     ),
+            //     Text(_x1Number.toString(),
+            //       textAlign: TextAlign.center,
+            //       overflow: TextOverflow.ellipsis,
+            //       style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children:  <Widget>[
+            //     Text('x2=',
+            //       textAlign: TextAlign.center,
+            //       overflow: TextOverflow.ellipsis,
+            //       style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+            //     ),
+            //     Text(_x2Number.toString(),
+            //       textAlign: TextAlign.center,
+            //       overflow: TextOverflow.ellipsis,
+            //       style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+            //     ),
+            //   ],
+            // ),
+
+
           ],
         ),
       ),
